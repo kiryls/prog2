301091d1d57976a1927b4e2a35611dd6c1632b15
Kiryl Shvachka <kiryl92@gmail.com>
2020-05-24 03:42:46 +0200

    insert throws limit error in Nil.java

--- lab10/Esercizi Liste/Nil.java
+++ lab10/Esercizi Liste/Nil.java
@@ -1,6 +1,9 @@
 public class Nil extends List {
     public List insert(int n, int x) {
         /* COMPLETARE */
+        if(x > 0)
+            throw new Error("Value exceeds the list bound");
+            
         return new Cons(n, this);
     }
 
