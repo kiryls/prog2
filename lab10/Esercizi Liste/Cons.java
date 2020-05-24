a695798657ca1a956220634667cb99f939428caf
kiryls <kiryl92@gmail.com>
2020-05-24 16:05:13 +0200

    fixed insert method

--- lab10/Esercizi Liste/Cons.java
+++ lab10/Esercizi Liste/Cons.java
@@ -1,18 +1,42 @@
+import java.util.Iterator;
+
 public class Cons extends List {
-    private int elem;   // Elemento memorizzato
-    private List next;  // Riferimento al nodo successore
+    private int elem;
+    private List next;
 
     public Cons(int elem, List next) {
         this.elem = elem;
         this.next = next;
     }
 
+    public int getElem() {
+        return elem;
+    }
+
+    public List getNext() {
+        return next;
+    }
+
     public String toString() {
         return elem + ", " + next.toString();
     }
 
-    public List insert(int n, int x) {
-        /* COMPLETARE */
-        return null;
+    public List insert(int n, int x) throws IllegalArgumentException {
+        if (x < 0)
+            throw new IllegalArgumentException("Negative index");
+
+        if (x == 0)
+            return new Cons(n, this);
+
+        try {
+            return this.next.insert(n, x - 1);
+        } catch (IllegalArgumentException e) {
+            System.err.println("Error: " + e);
+            return this;
+        }
+    }
+
+    public ListIterator iterator() {
+        return new ListIterator(this);
     }
 }
