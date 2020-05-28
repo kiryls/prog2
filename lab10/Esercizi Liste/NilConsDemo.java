51bdab28969a7db4786ae4d1e8c4bb7d6d7e9dce
Kiryl Shvachka <kiryl92@gmail.com>
2020-05-28 21:24:11 +0200

    updating demo class

--- lab10/Esercizi Liste/NilConsDemo.java
+++ lab10/Esercizi Liste/NilConsDemo.java
@@ -2,26 +2,31 @@ public class NilConsDemo {
     public static void main(String[] args) {
         List l = new Nil();
 
+        Title.show("iteratore su lista vuota");
+        for (Integer i : l)
+            System.out.println(i);
+
+        Title.show("inserimenti non ammessi");
         try {
            l = l.insert(2, -1);
         } catch (IllegalArgumentException e) {
-            System.err.println("Err: " + e);
+            System.err.println(e);
         }
 
-        for (Integer i : l)
-            System.out.println(i);
+        try {
+            l = l.insert(2, 42);
+        } catch (IllegalArgumentException e) {
+            System.err.println(e);
+        }
 
         Title.show("aggiungo elementi alla lista");
         l = l.insert(4, 0);
         l = l.insert(1, 0);
-        l = l.insert(3, 1); // 1 3 4
-        l = l.insert(-2, 42);
+        l = l.insert(3, 1);
 
         Title.show("list so far");
         for(Integer i : l)
-            System.out.println(i);
-
-        
+            System.out.println(i);   
        
     }
 }
