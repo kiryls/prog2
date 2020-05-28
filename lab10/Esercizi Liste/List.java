9d0b63240f6f9c8c077b5e50d5f17979e5e2239d
Kiryl Shvachka <kiryl92@gmail.com>
2020-05-28 21:06:15 +0200

    added getNext & empty methods

--- lab10/Esercizi Liste/List.java
+++ lab10/Esercizi Liste/List.java
@@ -1,4 +1,6 @@
 
 public abstract class List implements Iterable<Integer> {
     public abstract List insert(int n, int x);
+    public abstract List getNext();
+    public abstract boolean empty();
 }
