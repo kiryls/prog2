eaab36d8943e94129cd4dfd7fcea5b683ad3a2a2
Kiryl Shvachka <kiryl92@gmail.com>
2020-06-05 18:41:07 +0200

    beautified leaf

--- lab11/Leaf.java
+++ lab11/Leaf.java
@@ -6,38 +6,38 @@ public class Leaf extends Tree {
     public Leaf() { }
 
     public boolean empty() {
-	return true;
+	    return true;
     }
 
     public int max() {
-	// e` illegale invocare l'operazione max sull'albero vuoto...
-	assert false;
-	// ...tuttavia il compilatore mi costringe a ritornare
-	// comunque un valore perche' il metodo ha tipo di ritorno
-	// int
-	return 0;
+        // e` illegale invocare l'operazione max sull'albero vuoto...
+        assert false;
+        // ...tuttavia il compilatore mi costringe a ritornare
+        // comunque un valore perche' il metodo ha tipo di ritorno
+        // int
+        return 0;
     }
 
     public boolean contains(int x) {
-	return false;
+	    return false;
     }
 
     public Tree insert(int x) {
-	return new Branch(x, this, this);
+	    return new Branch(x, this, this);
     }
 
     public Tree remove(int x) {
-	return this;
+	    return this;
     }
 
     // realizziamo anche un metodo toString per stampare un albero
     // binario sul terminale
     public String toString() {
-	return "Leaf";
+	    return "Leaf";
     }
 
     public int depth() {
-	return 0;
+	    return 0;
     }
 
     // #############################################################
