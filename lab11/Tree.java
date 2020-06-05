2aed35e3ad587a1b8ffafaff95bc04fd46bf24ef
Kiryl Shvachka <kiryl92@gmail.com>
2020-06-05 13:40:40 +0200

    new methods in Tree class

--- lab11/Tree.java
+++ lab11/Tree.java
@@ -10,4 +10,12 @@ public abstract class Tree {
     public abstract Tree insert(int x);
     public abstract Tree remove(int x);
     public abstract int depth();
+
+    // nuovi metodi
+    public abstract int size();
+    public abstract int sum();
+    public abstract boolean contains(int x, int n);
+    public abstract boolean balanced();
+    public abstract Tree filter_le(int x);
+    public abstract int get(int i);
 }
